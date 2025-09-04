8edc981a10e30c302dcf04e0cdd84b4c4b67019b
Adam Jabłoński <adam.jab2004@gmail.com>
2025-09-04 17:32:19 +0200

    minor layout changes

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,22 +1,41 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import MapComponent from './MapComponent'
 import styles from '../styles/mainStyles.module.css'
 import LeafRender from './LeafRender'
 import Navbar from './Navbar'
+import Footer from './Footer'
+import Popup from './Popup'
 
 
-function main() {
+function Main() {
+
+  const[helpView, setHelpView] = useState(false)
+
+  useEffect(()=>{
+
+  },[helpView])
 
   return (
-    <div className={styles.body}>
+    <div className={styles.container}>
         <Navbar/>
 
+        {/* <div className={styles.footer}>
+          <Footer/>
+        </div> */}
+
+        {(helpView)? (<Popup/>): (null)}
+       
+       
+      
         <div className={styles.mapComponent}>
            {/* <MapComponent></MapComponent> */}
              <LeafRender></LeafRender>
         </div>
+       
+         <Footer/>
     </div>
   )
 }
 
-export default main
+export default Main
