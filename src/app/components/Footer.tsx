b28b6558879f63018412b690affeaa5e720dca65
Adam Jabłoński <adam.jab2004@gmail.com>
2025-09-04 17:30:59 +0200

    adding popup functionality (ongoing)

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import localFont from 'next/font/local'
+import Button from './Button'
+import styles from '../styles/footerStyles.module.css'
+import { popup } from 'leaflet'
+
+const font = localFont({
+        src: "../fonts/geograph/geographweblight.ttf",
+})
+
+function addToilet(){
+   console.log("add toilet");
+}
 
 function Footer() {
+      const[popup ,setPopup] = useState(false)
+
+      useEffect(()=>{
+        if(popup){
+          addToilet()
+        }
+      },[popup])
+
+  function openForm(){
+     
+      setPopup(true)
+  }
+  
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+       <Button font={font} text="Add toilet" click={openForm} btWidth='20vw'></Button>
+    </div>
+  
   )
 }
 
---
+++ src/app/playground/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Popup from "./components/Popup"
+
+function page() {
+  return (
+    <div>
+      <Popup></Popup>
+    </div>
+
+  )
+}
+
+export default page
