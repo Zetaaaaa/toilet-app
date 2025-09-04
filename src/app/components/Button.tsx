df86b6835902f9d1972cd02316b6bb2fb1767a24
Adam Jabłoński <adam.jab2004@gmail.com>
2025-09-04 17:29:56 +0200

    added more usage cases

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -3,19 +3,21 @@ import styles from "../styles/buttonStyles.module.css"
 import { NextFont } from 'next/dist/compiled/@next/font';
 
 
+
 //WHAT ARE INTERFACES AND TYPES
 interface ButtonProps {
   font: NextFont;
   text: string;
+  btWidth?: string;
   click: (text:string) => void;
 }
-function Button({font,text,click}:ButtonProps) {
+function Button({font,text,btWidth,click}:ButtonProps) {
   return (
     <div className={`${styles.navButton} ${font.className}`}
-
-      
+    // if btWidth is a prop overwrite module.css style
+    style={(btWidth != undefined)?{width:`${btWidth}`}:{}}
+   
     onClick={()=>click(text)}>
-
         <p>{text}</p>
     </div>
 
