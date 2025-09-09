8dc38911f031af456d9f2a47142fb0c06a1ccc3c
Adam Jabłoński <adam.jab2004@gmail.com>
2025-09-09 23:08:04 +0200

    Some cleanup done

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -5,30 +5,28 @@ import LeafRender from './LeafRender'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import NavbarNew from './NavbarNew'
-// import Popup from './Popup'
+
 
 
 function Main() {
 
     const[helpView, setHelpView] = useState(false)
     const [footerVisible, setVisible] = useState(false)
+    const [addToilet, setAddToilet] = useState(true)
 
   useEffect(()=>{
 
   },[helpView])
 
   return (
-    <div >
+    <div>
         <NavbarNew visible={setVisible}/>
-
-        {/* <div className={styles.footer}>
-          <Footer/>
-        </div> */}
         
         <div>
            {/* <MapComponent></MapComponent> */}
              <LeafRender></LeafRender>
         </div>
+
 
         {footerVisible?<Footer></Footer>:null}
        
