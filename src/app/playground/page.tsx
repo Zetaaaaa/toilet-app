c4011a1243e4541e7601d8fd0275220c58df4a94
Adam Jabłoński <adam.jab2004@gmail.com>
2025-09-12 23:00:26 +0200

    minor playground changes

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Form from "./components/Form"
+'use client'
+import { POST } from '../api/toilets/route'
 import Navbar from './components/Navbar'
+import { Button } from '@/components/ui/button'
+
+
+  async function getQuery() {
+    const response = await fetch('api/toilets',)
+    const result = await response.json()
+    console.log(result);
+  }
 
 function page() {
   return (
     <div>
       <Navbar></Navbar>
       <div className='font-geograph-light-italic'>There&apos;s not much here for now</div>
+      <Button onClick={()=>getQuery()}>witam</Button>
       
     </div>
 
