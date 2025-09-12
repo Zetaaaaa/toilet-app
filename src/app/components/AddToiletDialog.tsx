46640aa51a61de3cd49e1fcc96cd8a1562448f83
Adam Jabłoński <adam.jab2004@gmail.com>
2025-09-12 22:59:40 +0200

    Added search icons

--- src/app/components/AddToiletDialog.tsx
+++ src/app/components/AddToiletDialog.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Button } from "@/components/ui/button"
+import { Wallet,CircleQuestionMark, MapPinHouse, Toilet } from "lucide-react"
 import {
   Dialog,
   DialogClose,
@@ -10,7 +11,6 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from '@/components/ui/input'
-import { log } from 'console';
 import { Textarea } from '@/components/ui/textarea';
 
 interface FormProps{
@@ -34,9 +34,32 @@ function AddToiletDialog({getData}:FormProps) {
                 <DialogDescription className='font-geograph-light-italic'> You can add toilet point here - <span className='font-geograph-it-bold text-neutral-900 '>must be logged in to do that! </span></DialogDescription>
               </DialogHeader>
               <div className='flex flex-row w-1/1 justify-around gap-10'>
-                <Input type='text' onInput={(e)=>setToiletName(e.currentTarget.value)} className='font-geograph-light grow-1' placeholder='Name of the Toilet Point'></Input>
-                <Input type='text' onInput={(e)=>{setToiletAddress(e.currentTarget.value)}} className='font-geograph-light grow-2' placeholder='Address'></Input>
+              <div className='grow-1'>
+                <Toilet className='absolute h-9 left-9'></Toilet>
+                <Input type='text' onInput={(e)=>setToiletName(e.currentTarget.value)} className='font-geograph-light-bold grow-1 pl-10' placeholder='Name of the Toilet Point'></Input>
+
               </div>
+              <div className='grow-1'>
+                <MapPinHouse className='absolute h-9 left-71'></MapPinHouse>
+                <Input type='text' onInput={(e)=>{setToiletAddress(e.currentTarget.value)}} className='font-geograph-light-bold pl-10' placeholder='Address'></Input>
+              </div>
+             
+              </div>
+              <div className='flex flex-row w-1/1 justify-around gap-10'>
+
+                <div className='grow-1'>
+                  <Wallet className='absolute h-9 left-9'></Wallet>
+                  <Input type='text' placeholder='Price' className='font-geograph-light-bold pl-10'></Input>  
+                </div>
+                <div className='grow-1'>
+                  <CircleQuestionMark className='absolute h-9 left-71'></CircleQuestionMark>
+                  <Input type='text' placeholder='Placeholder' className='font-geograph-light-bold pl-10'></Input>
+                </div>
+
+               
+           
+              </div>
+                
 
                 <Textarea onInput={(e)=>setToiletDescription(e.currentTarget.value)} placeholder='Write something about the Toilet Point'></Textarea>
 
