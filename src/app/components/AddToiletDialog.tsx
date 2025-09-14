93290bd6046e531e81a7a0fcf53aa1bf5a37e83b
Adam Jabłoński <adam.jab2004@gmail.com>
2025-09-14 22:31:26 +0200

    added popover component

--- src/app/components/AddToiletDialog.tsx
+++ src/app/components/AddToiletDialog.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Button } from "@/components/ui/button"
-import { Wallet,CircleQuestionMark, MapPinHouse, Toilet } from "lucide-react"
+import { Wallet,CircleQuestionMark, MapPinHouse, Toilet, NotebookText } from "lucide-react"
 import {
   Dialog,
   DialogClose,
@@ -12,7 +12,14 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea';
-
+import { Separator } from '@/components/ui/separator'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Label } from '@/components/ui/label'
+import OpenTimesPopover from './OpenTimesPopover'
 interface FormProps{
   getData:(Name:string,Address:string, Description:string)=>void;
 }
@@ -33,6 +40,7 @@ function AddToiletDialog({getData}:FormProps) {
                 <DialogTitle className='font-geograph-light'> Add Toilet Point</DialogTitle>
                 <DialogDescription className='font-geograph-light-italic'> You can add toilet point here - <span className='font-geograph-it-bold text-neutral-900 '>must be logged in to do that! </span></DialogDescription>
               </DialogHeader>
+              <Separator className='bg-indigo-200'/>
               <div className='flex flex-row w-1/1 justify-around gap-10'>
               <div className='grow-1'>
                 <Toilet className='absolute h-9 left-9'></Toilet>
@@ -55,13 +63,33 @@ function AddToiletDialog({getData}:FormProps) {
                   <CircleQuestionMark className='absolute h-9 left-71'></CircleQuestionMark>
                   <Input type='text' placeholder='Placeholder' className='font-geograph-light-bold pl-10'></Input>
                 </div>
-
-               
-           
               </div>
                 
-
-                <Textarea onInput={(e)=>setToiletDescription(e.currentTarget.value)} placeholder='Write something about the Toilet Point'></Textarea>
+                <div>
+                  <NotebookText className='absolute h-10 left-9'></NotebookText>
+                  <Textarea onInput={(e)=>setToiletDescription(e.currentTarget.value)} className='font-geograph-light-bold pl-10' placeholder='Write something about the Toilet Point...'></Textarea>
+                </div>
+                  <Separator className='bg-indigo-200'/>
+                <div>
+                  <DialogDescription className='font-geograph-light-italic mb-2'>Add opening hours: </DialogDescription>
+                   
+                  <div className='flex flex-row h-10 justify-around'>
+                          <OpenTimesPopover day='Monday'></OpenTimesPopover>
+                            <Separator className='bg-indigo-300' orientation='vertical'/>
+                          <OpenTimesPopover day='Tuesday'></OpenTimesPopover>
+                            <Separator className='bg-indigo-300' orientation='vertical'/>
+                          <OpenTimesPopover day='Wedensday'></OpenTimesPopover>
+                            <Separator className='bg-indigo-300' orientation='vertical'/>
+                          <OpenTimesPopover day='Thursday'></OpenTimesPopover>
+                            <Separator className='bg-indigo-300' orientation='vertical'/>
+                          <OpenTimesPopover day='Friday'></OpenTimesPopover>
+                            <Separator className='bg-indigo-300' orientation='vertical'/>
+                          <OpenTimesPopover day='Saturday'></OpenTimesPopover>
+                            <Separator className='bg-indigo-300' orientation='vertical'/>
+                          <OpenTimesPopover day='Sunday'></OpenTimesPopover>
+                  </div>
+                </div>
+               
 
                 <DialogClose asChild>
                   <Button type="submit" onClick={()=>getData(toiletName,toiletAddress,toiletDescription)} className='justify-self-center w-25'>Confirm</Button>
